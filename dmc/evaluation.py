ef0afc59e464211ce60d424e478dac03a2d4be04
Alexander Immer <alexander.immer@yahoo.com>
2016-04-15 20:51:28 +0200

    Add cost function with tests

--- dmc/evaluation.py
+++ dmc/evaluation.py
@@ -5,3 +5,8 @@ def dmc_cost(predicted: np.array, ground_truth: np.array) -> int:
     """Cost function defined for the DMC"""
     diff = np.abs(predicted - ground_truth)
     return np.sum(diff)
+
+
+def dmc_cost_relative(predicted: np.array, ground_truth: np.array) -> int:
+    cost = dmc_cost(predicted, ground_truth)
+    return cost/len(cost)
---
+++ tests/test_evaluation.py
@@ -0,0 +1,17 @@
+import unittest
+import numpy as np
+from dmc.evaluation import dmc_cost, dmc_cost_relative
+
+
+class DMCCostTest(unittest.TestCase):
+    def setUp(self):
+        self.a = np.array([1, 2, 3, 4, 5])
+        self.b = np.array([0, 2, 4, 4, 3])
+
+    def test_dmc_cost(self):
+        res = dmc_cost(self.a, self.b)
+        self.assertEqual(res, 4)
+
+    def test_dmc_relative(self):
+        res = dmc_cost_relative(self.a ,self.b)
+        self.assertEqual(res, 0.8)
