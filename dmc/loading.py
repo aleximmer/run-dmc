f46ad7469f44d72d303e94e0fdd7b5f6eb665639
Alexander Immer <alexander.immer@yahoo.com>
2016-04-17 19:57:41 +0200

    Add temporal features

--- dmc/loading.py
+++ dmc/loading.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+
 def data_train():
     return pd.read_csv('data/orders_train.txt', sep=';')
+
 
 def data_class():
     return pd.read_csv('data/orders_class.txt', sep=';')
--- dmc/preprocessing.py
+++ dmc/preprocessing.py
@@ -8,6 +8,17 @@ def encode_features(df: pd.DataFrame) -> pd.DataFrame:
 
 def add_features(df: pd.DataFrame) -> pd.DataFrame:
     """Add new features to the DataFrame"""
+    df['productPrice'] = df.price / df.quantity
+    df['totalSavings'] = df.rrp - df.productPrice
+    df['relativeSavings'] = 1 - df.productPrice/df.rrp
+    df['orderYear'] = df.orderDate.apply(lambda x: x.year)
+    df['orderMonth'] = df.orderDate.apply(lambda x: x.month)
+    df['orderDay'] = df.orderDate.apply(lambda x: x.day)
+    df['orderWeekDay'] = df.orderDate.apply(lambda x: x.dayofweek)
+    df['orderDayOfYear'] = df.orderDate.apply(lambda x: x.dayofyear)
+    df['orderWeek'] = df.orderDate.apply(lambda x: x.week)
+    df['orderWeekOfYear'] = df.orderDate.apply(lambda x: x.weekofyear)
+    df['orderQuarter'] = df.orderDate.apply(lambda x: x.quarter)
     return df
 
 
