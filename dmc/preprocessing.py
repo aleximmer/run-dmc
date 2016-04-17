feb2947966548cdd6c5795ef3c9569f5e8205b2a
fawind <windheuser.fabian@gmail.com>
2016-04-17 20:59:11 +0200

    Add season feature

--- dmc/preprocessing.py
+++ dmc/preprocessing.py
@@ -24,3 +24,17 @@ def customer_return_probability(df: pd.DataFrame) -> pd.DataFrame:
     df['customerReturnProbs'] = customer_return_probs.loc[df['customerID']]
     return df
 
+
+def season(df: pd.DataFrame) -> pd.DataFrame:
+    def date_to_season(date):
+        if date.month <= 3 and date.day <= 22:
+            return 1
+        if date.month <= 6 and date.day <= 22:
+            return 2
+        if date.month <= 9 and date.day <= 22:
+            return 3
+        if date.month <= 12 and date.day <= 22:
+            return 4
+        return 1
+    df['season'] = df.orderDate.copy().apply(date_to_season)
+    return df
