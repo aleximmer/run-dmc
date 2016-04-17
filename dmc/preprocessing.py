e8ace2c71142293aafe5b264460e393c4fd45dfb
Alexander Immer <alexander.immer@yahoo.com>
2016-04-17 22:00:33 +0200

    Same article surplus

--- dmc/preprocessing.py
+++ dmc/preprocessing.py
@@ -19,33 +19,39 @@ def add_features(df: pd.DataFrame) -> pd.DataFrame:
     df['orderWeek'] = df.orderDate.apply(lambda x: x.week)
     df['orderWeekOfYear'] = df.orderDate.apply(lambda x: x.weekofyear)
     df['orderQuarter'] = df.orderDate.apply(lambda x: x.quarter)
+    df['orderSeason'] = df.orderDate.apply(date_to_season)
     df = customer_return_probability(df)
+    df = same_article_surplus(df)
     return df
+
+
+def customer_return_probability(df: pd.DataFrame) -> pd.DataFrame:
+    customer_return_probs = (df.groupby(['customerID']).returnQuantity.sum() /
+                             df.groupby(['customerID']).quantity.sum())
+    df['customerReturnProbs'] = customer_return_probs.loc[df.customerID]
+    return df
+
+
+def same_article_surplus(df: pd.DataFrame) -> pd.DataFrame:
+    article_group = df.groupby(['orderID', 'articleID'])['quantity'].sum()
+    index = list(zip(df.orderID, df.articleID))
+    df['surplusQuantity'] = list(article_group.loc[index]) - df.quantity
+    return df
+
+
+def date_to_season(date):
+    if date.month <= 3 and date.day <= 22:
+        return 1
+    if date.month <= 6 and date.day <= 22:
+        return 2
+    if date.month <= 9 and date.day <= 22:
+        return 3
+    if date.month <= 12 and date.day <= 22:
+        return 4
+    return 1
 
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df = add_features(df)
     df = encode_features(df)
-    return df
-
-
-def customer_return_probability(df: pd.DataFrame) -> pd.DataFrame:
-    customer_return_probs = (df.groupby(['customerID'])['returnQuantity'].sum() /
-                             df.groupby(['customerID'])['quantity'].sum())
-    df['customerReturnProbs'] = customer_return_probs.loc[df['customerID']]
-    return df
-
-
-def season(df: pd.DataFrame) -> pd.DataFrame:
-    def date_to_season(date):
-        if date.month <= 3 and date.day <= 22:
-            return 1
-        if date.month <= 6 and date.day <= 22:
-            return 2
-        if date.month <= 9 and date.day <= 22:
-            return 3
-        if date.month <= 12 and date.day <= 22:
-            return 4
-        return 1
-    df['season'] = df.orderDate.copy().apply(date_to_season)
     return df
