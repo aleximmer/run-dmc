cbcdcc0296e28200dfe8289ebb85dc00cfaf073a
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2016-04-17 20:54:44 +0200

    Add customerReturnProbability

--- dmc/preprocessing.py
+++ dmc/preprocessing.py
@@ -8,6 +8,7 @@ def encode_features(df: pd.DataFrame) -> pd.DataFrame:
 
 def add_features(df: pd.DataFrame) -> pd.DataFrame:
     """Add new features to the DataFrame"""
+    df = customer_return_probability(df)
     return df
 
 
@@ -15,3 +16,11 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df = add_features(df)
     df = encode_features(df)
     return df
+
+
+def customer_return_probability(df: pd.DataFrame) -> pd.DataFrame:
+    customer_return_probs = (df.groupby(['customerID'])['returnQuantity'].sum() /
+                             df.groupby(['customerID'])['quantity'].sum())
+    df['customerReturnProbs'] = customer_return_probs.loc[df['customerID']]
+    return df
+
