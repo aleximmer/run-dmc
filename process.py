2ce2ea918bc54bfac3697e92930cb3e154618e45
Alex Immer <alexander.immer@yahoo.com>
2016-04-22 21:34:10 +0200

    Correct filepath for persistent csv saving

--- process.py
+++ process.py
@@ -10,7 +10,7 @@ def eval_classifiers(df: pd.DataFrame, tr_size, te_size):
     # shuffle Dataframe
     df = df.reindex(np.random.permutation(df.index))
     df = df[:te_size + tr_size]
-    X, Y = dmc.transformation.transform(df, scaler=None)
+    X, Y = dmc.transformation.transform(df, scaler=dmc.normalization.normalize_features)
     train = X[:tr_size], Y[:tr_size]
     test = X[tr_size:tr_size + te_size], Y[tr_size:tr_size + te_size]
     for classifier in dmc.classifiers.DMCClassifier.__subclasses__():
@@ -21,7 +21,7 @@ def eval_classifiers(df: pd.DataFrame, tr_size, te_size):
 
 
 def processed_data() -> pd.DataFrame:
-    rel_file_path = os.path.join(os.path.dirname(__file__) + processed_file)
+    rel_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)) + processed_file)
     if os.path.isfile(rel_file_path):
         return pd.DataFrame.from_csv(rel_file_path)
     data = dmc.data_train()
