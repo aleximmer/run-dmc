ce55f22ceacef9dbba7a038c300b4786d0b0e95e
Alexander Immer <alexander.immer@yahoo.com>
2016-04-23 12:30:29 +0200

    Remove encoding from module import

--- dmc/__init__.py
+++ dmc/__init__.py
@@ -4,5 +4,4 @@ import dmc.preprocessing
 import dmc.classifiers
 import dmc.evaluation
 import dmc.transformation
-import dmc.encoding
 import dmc.normalization
--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 import numpy as np
-import dmc
+import dmc.encoding
 
 
 class EncodingTest(unittest.TestCase):
