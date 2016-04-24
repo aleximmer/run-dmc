95d4f07208b3482b67351415ed7cccae44a944c6
Alexander Immer <alexander.immer@yahoo.com>
2016-04-24 18:51:28 +0200

    Use sparse representation on dataframe to matrices

--- dmc/transformation.py
+++ dmc/transformation.py
@@ -1,7 +1,8 @@
 import numpy as np
+from scipy.sparse import csr_matrix, hstack
 import pandas as pd
 
-from dmc.encoding import encode_features
+from dmc.encoding import encode_features, encode_features_np
 
 
 target_feature = 'returnQuantity'
@@ -14,19 +15,19 @@ def transform_preserving_headers(df: pd.DataFrame) -> \
     """Transform specific data space and return list of tuples indicating where features lie"""
     X, ft_list = np.empty((len(df), 0)), []
     for ft in [ft for ft in df.columns if ft not in default_ignore_features]:
-        X_enc = encode_features(df, ft)
-        X = np.append(X, encode_features(df, ft), axis=1)
+        X_enc = encode_features_np(df, ft)
+        X = np.append(X, encode_features_np(df, ft), axis=1)
         ft_list.extend([ft] * len(X_enc.T))
     return X.astype(np.float32), np.array(ft_list)
 
 
-def transform_feature_matrix(df: pd.DataFrame, ignore_features: list) -> np.array:
+def transform_feature_matrix(df: pd.DataFrame, ignore_features: list) -> csr_matrix:
     """Used to transform the full data space in order to get all categories"""
     assert target_feature in ignore_features
-    X = np.empty((len(df), 0))
+    X = None
     for ft in [ft for ft in df.columns if ft not in ignore_features]:
-        X = np.append(X, encode_features(df, ft), axis=1)
-    return X.astype(np.float32)
+        X = encode_features(df, ft) if X is None else hstack([X, encode_features_np(df, ft)])
+    return X.astype(np.float64)
 
 
 def transform_target_vector(df: pd.DataFrame) -> np.array:
