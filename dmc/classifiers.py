d333948adaf61c5b6f9a2af7d3e1b34ad8b1348a
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-04-26 13:58:43 +0200

    shrink number of possibilities

--- dmc/classifiers.py
+++ dmc/classifiers.py
@@ -51,10 +51,10 @@ class DMCClassifier:
 
     def estimate_parameters_with_grid_search_cv(self):
         # use a full grid over all parameters
-        param_grid = {"max_depth": [None, 10, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
-                      "max_features": [None, 1, 10, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
-                      "min_samples_split": [1, 10, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
-                      "min_samples_leaf": [1, 10, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+        param_grid = {"max_depth": [None, 1, 10, 100],
+                      "max_features": [None, 1, 10, 100],
+                      "min_samples_split": [1, 5, 10, 40, 50, 100],
+                      "min_samples_leaf": [1, 10, 100],
                       "criterion": ["gini", "entropy"]}
 
         # run grid search
