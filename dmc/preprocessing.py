c06949a8321d9d4d8656f9667a255dfa62061b23
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2016-04-30 00:25:08 +0200

    Add dynamic colorCode binning #58

--- dmc/preprocessing.py
+++ dmc/preprocessing.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import holidays
 
+COLOR_CODE_MIN = 0
+COLOR_CODE_MAX = 9999
+
 
 def add_features(df: pd.DataFrame) -> pd.DataFrame:
     """Add features to DataFrame.
@@ -27,8 +30,8 @@ def add_features(df: pd.DataFrame) -> pd.DataFrame:
     df['orderDayOfYear'] = df.orderDate.apply(lambda x: x.dayofyear)
     df['orderWeek'] = df.orderDate.apply(lambda x: x.week)
     df['orderWeekOfYear'] = df.orderDate.apply(lambda x: x.weekofyear)
-    df['orderTotalDay'] = df.orderDate.apply(lambda x: x.dayofyear if x.year == 2014
-                                             else x.dayofyear + 365)
+    df['orderTotalDay'] = df.orderDate.apply(lambda x:
+                                             x.dayofyear if x.year == 2014 else x.dayofyear + 365)
     df['orderQuarter'] = df.orderDate.apply(lambda x: x.quarter)
     df['orderSeason'] = df.orderDate.apply(date_to_season)
     df['orderIsOnGermanHoliday'] = df.orderDate.apply(lambda x: 1 if x in holidays.DE() else 0)
@@ -41,6 +44,7 @@ def add_features(df: pd.DataFrame) -> pd.DataFrame:
     df['surplusArticleColorQuantity'] = same_article_same_color_surplus(df)
     df['totalOrderShare'] = total_order_share(df)
     df['voucherSavings'] = voucher_saving(df)
+    df['binnedColorCode'] = binned_color_code(df)
     return df
 
 
@@ -126,3 +130,54 @@ def merge_features(df: pd.DataFrame, feature_dfs: list) -> pd.DataFrame:
         feature_df.drop(conflicting_keys, inplace=True, axis=1)
         df = pd.merge(df, feature_df, how='left', on=unique_keys)
     return df
+
+
+def binned_color_code(df: pd.DataFrame, deviations=1.0) -> pd.DataFrame:
+    """Bin colorCode column.
+
+    This is to deal with unknown colorCodes (CC's) in the target set by binning the CC range.
+    The binning considers outlier CC's by keeping them separate, 1-sized bins.
+    Outliers are CC's whose return probability is over one standard deviation away from the mean.
+    For our training data (mean: 0.548, std: 0.114) colorCode c is an is an outlier if
+    
+        retProb(c) < 0.434 || 0.662 < retProb(c), given deviations = 1.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Table containing 'colorCodes' column to be binned
+    deviations : float
+        Number of standard deviations a return probability has to differ from the mean to be
+        considered an outlier.
+
+    Returns
+    -------
+    pd.Series
+        Series with a bin assigned to each row
+
+    """
+
+    def ret_prob(d: pd.Series) -> np.float64:
+        return d.astype(bool).sum() / len(d)
+
+    cc_ret_probs = df.groupby('colorCode')['returnQuantity'].apply(ret_prob)
+
+    mean = cc_ret_probs.mean()
+    diff = cc_ret_probs.std() * deviations
+
+    mean_distances = cc_ret_probs.sub(mean).abs()
+
+    bins = [COLOR_CODE_MIN]
+
+    # iterate over colorCodes and respective mean distances to collect bins
+    for cc, mean_distance in mean_distances.items():
+        if mean_distance > diff:
+            # add the colorCode as 1-sized bin (current cc and cc + 1)
+            # if the last colorCode was added, don't add the current cc, only cc + 1.
+            if bins[-1] != cc:
+                bins.append(cc)
+            bins.append(cc + 1)
+    bins.append(COLOR_CODE_MAX + 1)
+
+    cut = list(pd.cut(df.colorCode, bins, right=False))
+    return pd.Series(cut, index=df.index)
