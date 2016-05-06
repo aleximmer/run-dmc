9171ae5e13d063f865aa5d7a9e2205b9e75bdd29
Alexander Immer <ext-alexander.immer@here.com>
2016-05-06 19:41:41 +0200

    Refine split and correct bugs

--- dmc/ensemble.py
+++ dmc/ensemble.py
@@ -15,15 +15,21 @@ def add_recognition_vector(train: pd.DataFrame, test: pd.DataFrame, columns: lis
         unknown = unknown_categories(col, train, test)
         column_name = 'known' + col
         recognition_columns.append(column_name)
-        test[column_name] = test.apply(lambda x: x not in unknown)
+        test[column_name] = test[col].apply(lambda x: x not in unknown)
     return test, recognition_columns
 
 
 def split(train: pd.DataFrame, test: pd.DataFrame) -> list():
     """For each permutation of known and unknown for specific categories
     return the cropped train DataFrame and the test subset for evaluation"""
-    test, splitters = add_recognition_vector(train, test)
+    potentially_unknown = ['articleID', 'customerID', 'voucherID', 'productGroup']
+    test, splitters = add_recognition_vector(train, test, potentially_unknown)
     result = []
-    for split, group in test.groupby(splitters):
-        result.append((train, group, split))
+    for vec, group in test.groupby(splitters):
+        specifier = '-'.join(['known' + str(e[1]) if e[0] else 'unknown' + str(e[1])
+                              for e in zip(vec, potentially_unknown)])
+        unknown = [e[1] for e in zip(vec, potentially_unknown) if not e[0]]
+        train_crop = train.copy().drop(unknown, axis=1)
+        test_group = group.copy().drop(unknown + splitters, axis=1)
+        result.append((train_crop, test_group, specifier))
     return result
