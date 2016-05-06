2267118c02dbbb089f1f18c43203a6e32dc48e5d
Alexander Immer <ext-alexander.immer@here.com>
2016-05-06 21:54:33 +0200

    Revert partially to old loading structure

--- process.py
+++ process.py
@@ -1,7 +1,6 @@
 import os.path
 import argparse
 import pandas as pd
-import numpy as np
 
 import dmc
 from dmc.classifiers import DecisionTree, Forest, NaiveBayes, SVM, NeuralNetwork
@@ -9,23 +8,19 @@ from dmc.classifiers import TreeBag, BayesBag, SVMBag
 from dmc.classifiers import AdaTree, AdaBayes, AdaSVM
 
 
+processed_file = '/data/processed.csv'
+
 # Remove classifiers which you don't want to run and add new ones here
 basic = [DecisionTree, Forest, NaiveBayes, SVM, NeuralNetwork]
 bag = [TreeBag, BayesBag, SVMBag]
 ada = [AdaTree, AdaBayes, AdaSVM]
 
 
-def shuffle(df: pd.DataFrame) -> pd.DataFrame:
-    return df.reindex(np.random.permutation(df.index))
-
-
-def eval_classifiers(df: pd.DataFrame, tr_size, te_size):
-    df = shuffle(df)
-    df = df[:te_size + tr_size]
+def eval_classifiers(df: pd.DataFrame, split: int):
     X, Y = dmc.transformation.transform(df, scaler=dmc.transformation.scale_features,
                                         binary_target=True)
-    train = X[:tr_size], Y[:tr_size]
-    test = X[tr_size:tr_size + te_size], Y[tr_size:tr_size + te_size]
+    train = X[:split], Y[:split]
+    test = X[split:], Y[split:]
     for classifier in (basic + bag + ada):
         clf = classifier(train[0], train[1])
         res = clf(test[0])
@@ -33,18 +28,17 @@ def eval_classifiers(df: pd.DataFrame, tr_size, te_size):
         print(precision, ' using ', str(classifier))
 
 
-def eval_features(df: pd.DataFrame, size):
-    df = shuffle(df)
-    ft_importance = dmc.evaluation.evaluate_features_by_ensemble(df[:size])
+def eval_features(df: pd.DataFrame):
+    ft_importance = dmc.evaluation.evaluate_features_by_ensemble(df)
     print(ft_importance.sort_values('tree', ascending=False))
 
 
-def processed_data(id_file_prefix: str) -> pd.DataFrame:
+def processed_data() -> pd.DataFrame:
     """Create or read DataFrame with all features that are independent"""
-    rel_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)) + 'processed.csv')
+    rel_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)) + processed_file)
     if os.path.isfile(rel_file_path):
         return pd.read_csv(rel_file_path)
-    df = dmc.data_train()
+    df = dmc.loading.data_train()
     df = dmc.preprocessing.cleanse(df)
     df = dmc.features.add_independent_features(df)
     print('Finished processing. Dumping results to {}.'.format(rel_file_path))
@@ -52,9 +46,12 @@ def processed_data(id_file_prefix: str) -> pd.DataFrame:
     return df
 
 
-def split_data_by_id(df: pd.DataFrame, id_file_prefix: str) -> pd.DataFrame:
+def split_data_by_id(df: pd.DataFrame, id_file_prefix: str) -> (pd.DataFrame, int):
     train_ids, test_ids = dmc.loading.load_ids(id_file_prefix)
     train, test = dmc.preprocessing.split_train_test(df, train_ids, test_ids)
+    #dmc.features.add_dependent_features(train)
+    # TODO: also add those values to test
+    return pd.concat([train, test]), len(train)
 
 
 if __name__ == '__main__':
@@ -64,5 +61,7 @@ if __name__ == '__main__':
     id_prefix = args.id_prefix
 
     data = processed_data()
-    eval_classifiers(data, 5000, 5000)
-    eval_features(data, 5000)
+    data, split_point = split_data_by_id(data, id_prefix)
+
+    eval_classifiers(data, split_point)
+    eval_features(data[:split_point])
