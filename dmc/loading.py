13c0dc734b1bb78066a344578f49c547ea01bd66
Alexander Immer <ext-alexander.immer@here.com>
2016-05-06 21:53:49 +0200

    Read strings without problematic ticks

--- dmc/loading.py
+++ dmc/loading.py
@@ -16,7 +16,7 @@ def load_ids(id_file_prefix: str) -> (list, list):
     train_path = 'data/idLists/' + id_file_prefix + 'Training.txt'
     test_path = 'data/idLists/' + id_file_prefix + 'Test.txt'
     with open(train_path, 'r') as f:
-        train_ids = [line.strip() for line in f]
+        train_ids = [line.strip().replace("\"", '') for line in f]
     with open(test_path, 'r') as f:
-        test_ids = [line.strip() for line in f]
+        test_ids = [line.strip().replace("\"", '') for line in f]
     return train_ids, test_ids
