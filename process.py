20a4b6eaa6d7ddae8ea3f8ef5029205bd0438417
marcus pappik <marcus.pappik@student.hpi.de>
2016-05-09 08:05:07 +0200

    load featuresets from file

--- process.py
+++ process.py
@@ -11,27 +11,36 @@ from dmc.classifiers import AdaTree, AdaBayes, AdaSVM
 processed_file = '/data/processed.csv'
 
 # Remove classifiers which you don't want to run and add new ones here
-basic = [DecisionTree, Forest, NaiveBayes, SVM, NeuralNetwork]
-bag = [TreeBag, SVMBag]
-ada = [AdaTree, AdaBayes, AdaSVM]
+classifierSet = {'Forest' : Forest, 'NaiveBayes' : NaiveBayes, 'SVM' : SVM, 'TreeBag' : TreeBag, 'AdaTree' : AdaTree, 'AdaBayes' : AdaBayes}
 
 
 def shuffle(df: pd.DataFrame) -> pd.DataFrame:
     return df.reindex(np.random.permutation(df.index))
 
 
+def get_fts_set(classifier_name, fts):
+    result_set = []
+    df = pd.read_csv('gen_algo/Sets_' + classifier_name +'_0.csv', sep=',', na_values='\\N')
+    df_sort = df.sort_values('accuracy', ascending=False)
+    for ft in fts:
+        if(ft in df_sort.columns.tolist() and df_sort.loc[0, ft] == 1):
+            result_set.append(ft)
+    return result_set		
+
+
 def eval_classifiers(df: pd.DataFrame, tr_size, te_size, tune_parameters):
-    df = shuffle(df)
+    df = shuffle(df)    
     df = df[:te_size + tr_size]
-    X, Y = dmc.transformation.transform(df, scaler=dmc.transformation.scale_features,
+    for name, classifier in classifierSet.items():
+        fts = get_fts_set(name, df.columns.tolist())        	
+        X, Y = dmc.transformation.transform(df.loc[:,fts + ['returnQuantity']], scaler=dmc.transformation.scale_features,
                                         binary_target=True)
-    train = X[:tr_size], Y[:tr_size]
-    test = X[tr_size:tr_size + te_size], Y[tr_size:tr_size + te_size]
-    for classifier in (basic + bag + ada):
+        train = X[:tr_size], Y[:tr_size]
+        test = X[tr_size:tr_size + te_size], Y[tr_size:tr_size + te_size]
         clf = classifier(train[0], train[1], tune_parameters)
         res = clf(test[0])
         precision = dmc.evaluation.precision(res, test[1])
-        print(precision, ' using ', str(classifier))
+        print(precision, ' using ', name)
 
 
 def eval_features(df: pd.DataFrame, size):
