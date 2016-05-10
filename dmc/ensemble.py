0e755a750cf2ca4a93b33df8371cdb391e9cf4ea
Alex Immer <AlexImmer@users.noreply.github.com>
2016-05-10 19:38:35 +0200

    Ensemble multiprocessing (#155)
    * Extend ensemble by mp
    
    * Update and mp ensemble
    
    * Fast run scripts
    
    * New id lists
    
    * Rename id list
    
    * Fast run scripts
    
    * Evaluate mirrored
    
    * Remove debug logging
    
    * Revert process.py to master

--- dmc/ensemble.py
+++ dmc/ensemble.py
@@ -1,9 +1,10 @@
+from multiprocessing import Pool, cpu_count
 import pandas as pd
 import numpy as np
 
 from dmc.transformation import normalize_features
 from dmc.transformation import transform
-from dmc.classifiers import Forest
+from dmc.classifiers import DecisionTree
 from dmc.evaluation import precision, dmc_cost
 
 
@@ -42,29 +43,55 @@ class Ensemble:
         self.train = train
         self.test = test
         self.splits = split(train, test)
+        self.pool = Pool(processes=3)
 
-    def transform(self, binary_target=True, scalers=None):
+    def transform(self, binary_target=True, scalers=None, ignore_features=None):
         scalers = [normalize_features] * len(self.splits) if scalers is None else scalers
-        for s, scaler in zip(self.splits, scalers):
-            offset = len(self.splits[s][0])
-            data = pd.concat([self.splits[s][0], self.splits[s][1]])
-            X, Y = transform(data, binary_target=binary_target, scaler=scaler)
-            self.splits[s] = ({
-                'train': (X[:offset], Y[:offset]),
-                'test': (X[offset:], Y[offset:])
-            })
+        ignore_features = ignore_features if ignore_features else [None] * len(self.splits)
+        binary = [binary_target] * len(self.splits)
 
-    def classify(self, classifiers=None):
-        results = []
-        classifiers = [Forest] * len(self.splits) if classifiers is None else classifiers
-        for s, classifier in zip(self.splits, classifiers):
-            clf = classifier(*self.splits[s]['train'])
-            pred = clf(self.splits[s]['test'][0])
-            prec = precision(pred, self.splits[s]['test'][1])
-            cost = dmc_cost(pred, self.splits[s]['test'][1])
-            results.append((len(self.splits[s]['test'][1]), prec, cost))
+        transformation_tuples = zip(self.splits.items(), scalers, ignore_features, binary)
+        trans = self.pool.map(self._transform_split, transformation_tuples)
+        for res in trans:
+            self.splits[res[0]] = res[1]
+
+    @staticmethod
+    def _transform_split(args):
+        item, scaler, rm_features, binary_target = args
+        offset = len(item[1][0])
+        data = pd.concat([item[1][0], item[1][1]])
+        if rm_features:
+            data = data.drop(rm_features, 1)
+        X, Y = transform(data, binary_target=binary_target, scaler=scaler)
+        return item[0], {
+            'train': (X[:offset], Y[:offset]),
+            'test': (X[offset:], Y[offset:])}
+
+    def classify(self, classifiers=None, hyper_param=False, verbose=True):
+        classifiers = [DecisionTree] * len(self.splits) if classifiers is None else classifiers
+        split_apply = zip(self.splits.items(), classifiers, [hyper_param] * len(self.splits))
+
+        results = self.pool.map(self._classify_split, split_apply)
+
         all_prec, all_cost, full_size = 0, 0, len(self.test)
-        for size, prec, cost in results:
+        for size, prec, cost, name, importances in results:
             all_prec += size / full_size * prec
             all_cost += cost
-        print(all_prec, all_cost)
+            if verbose:
+                print(name, ': size ', size, ' prec ', prec)
+                print('--------------------------------------')
+
+        print('Overall :', all_prec, all_cost)
+
+    @staticmethod
+    def _classify_split(args):
+        split, classifier, hyper_param = args
+        clf = classifier(*split[1]['train'], tune_parameters=hyper_param)
+        pred = clf(split[1]['test'][0])
+        prec = precision(pred, split[1]['test'][1])
+        cost = dmc_cost(pred, split[1]['test'][1])
+        try:
+            importances = clf.clf.feature_importances_
+        except:
+            importances = None
+        return len(split[1]['test'][1]), prec, cost, split[0], importances
