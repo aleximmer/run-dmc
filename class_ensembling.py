af235845c1bee8f21970b600094b399f09a84171
Alexander Immer <alexander.immer@yahoo.com>
2016-05-14 12:33:00 +0200

    Update ensembles wrt latest results

--- class_ensembling.py
+++ class_ensembling.py
@@ -1,14 +1,13 @@
 from process import processed_data
 from dmc.ensemble import ECEnsemble
-from dmc.classifiers import Forest, SVM, TheanoNeuralNetwork
+from dmc.classifiers import Forest, SVM
 from dmc.classifiers import TensorFlowNeuralNetwork as TensorNetwork
-from dmc.classifiers import NaiveBayes as Bayes
 from dmc.transformation import scale_features as scaler
 from dmc.transformation import scale_raw_features as raw_scaler
 from dmc.features import add_dependent_features
 
 
-""" Train on 100%, Classify on class data set """
+""" Train on 100% (sampled), Classify on class data set """
 
 data = processed_data(load_full=True)
 train = data[~data.returnQuantity.isnull()]
@@ -19,26 +18,26 @@ train, test = add_dependent_features(train, test)
 del data
 
 params = {
-    'uuuu': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'uuuk': {'sample': None, 'scaler': scaler, 'ignore_features': None, 'classifier': Bayes},
-    'uuku': {'sample': 200000, 'scaler': scaler, 'ignore_features': ['returnQuantity', 'orderID', 'orderDate'], 'classifier': TensorNetwork},
-    'uukk': {'sample': 100000, 'scaler': raw_scaler, 'ignore_features': None, 'classifier': SVM},
-    'ukuu': {'sample': 200000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'ukuk': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'ukku': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'ukkk': {'sample': 200000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kuuu': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'kuuk': {'sample': 200000, 'scaler': raw_scaler, 'ignore_features': None, 'classifier': TheanoNeuralNetwork},
-    'kuku': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'kukk': {'sample': 300000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'kkuu': {'sample': 200000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kkuk': {'sample': 250000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kkku': {'sample': 250000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kkkk': {'sample': 300000, 'scaler': None, 'ignore_features': None, 'classifier': Forest}
+    # article, customer, productGroup
+    'uuuu': {'sample': 500000, 'scaler': scaler, 'classifier': TensorNetwork},
+    'uuuk': {'sample': 100000, 'scaler': raw_scaler, 'classifier': SVM},
+    'uuku': {'sample': 500000, 'scaler': scaler, 'classifier': TensorNetwork},
+    'uukk': {'sample': 100000, 'scaler': raw_scaler, 'classifier': SVM},
+    'ukuu': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'ukuk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'ukku': {'sample': 500000, 'scaler': scaler, 'classifier': Forest},
+    'ukkk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'kuuk': {'sample': 100000, 'scaler': raw_scaler, 'classifier': SVM},
+    'kukk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'kkuk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'kkkk': {'sample': 750000, 'scaler': None, 'classifier': Forest}
 }
+
+for k in params:
+    params[k]['ignore_features'] = None
 
 ensemble = ECEnsemble(train, test, params)
 print('transform for class')
 ensemble.transform()
 print('classify for class')
-ensemble.classify(dump_results=True, dump_name='class')
+ensemble.classify(dump_results=True, dump_name='quadclass-ensemble')
--- test_ensembling.py
+++ test_ensembling.py
@@ -1,13 +1,13 @@
 from process import processed_data, split_data_at_id
 from dmc.ensemble import ECEnsemble
-from dmc.classifiers import Forest, SVM, TheanoNeuralNetwork
+from dmc.classifiers import Forest, SVM
 from dmc.classifiers import TensorFlowNeuralNetwork as TensorNetwork
 from dmc.classifiers import NaiveBayes as Bayes
 from dmc.transformation import scale_features as scaler
 from dmc.transformation import scale_raw_features as raw_scaler
 
 
-""" Train on 70%, Test on 30% and save test set """
+""" Train on 70% (sampled), Test on 30% and save test set """
 
 data = processed_data(load_full=True)
 data = data[~data.returnQuantity.isnull()]
@@ -17,23 +17,23 @@ train, test = split_data_at_id(data, 1527394)
 del data
 
 params = {
-    'uuuu': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'uuuk': {'sample': None, 'scaler': scaler, 'ignore_features': None, 'classifier': Bayes},
-    'uuku': {'sample': 200000, 'scaler': scaler, 'ignore_features': ['returnQuantity', 'orderID', 'orderDate'], 'classifier': TensorNetwork},
-    'uukk': {'sample': 100000, 'scaler': raw_scaler, 'ignore_features': None, 'classifier': SVM},
-    'ukuu': {'sample': 200000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'ukuk': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'ukku': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'ukkk': {'sample': 200000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kuuu': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'kuuk': {'sample': 200000, 'scaler': raw_scaler, 'ignore_features': None, 'classifier': TheanoNeuralNetwork},
-    'kuku': {'sample': 200000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'kukk': {'sample': 300000, 'scaler': scaler, 'ignore_features': None, 'classifier': TensorNetwork},
-    'kkuu': {'sample': 200000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kkuk': {'sample': 250000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kkku': {'sample': 250000, 'scaler': None, 'ignore_features': None, 'classifier': Forest},
-    'kkkk': {'sample': 300000, 'scaler': None, 'ignore_features': None, 'classifier': Forest}
+    # article, customer, productGroup
+    'uuuu': {'sample': 500000, 'scaler': scaler, 'classifier': TensorNetwork},
+    'uuuk': {'sample': 100000, 'scaler': raw_scaler, 'classifier': SVM},
+    'uuku': {'sample': 500000, 'scaler': scaler, 'classifier': TensorNetwork},
+    'uukk': {'sample': 100000, 'scaler': raw_scaler, 'classifier': SVM},
+    'ukuu': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'ukuk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'ukku': {'sample': 500000, 'scaler': scaler, 'classifier': Forest},
+    'ukkk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'kuuk': {'sample': 100000, 'scaler': raw_scaler, 'classifier': SVM},
+    'kukk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'kkuk': {'sample': 500000, 'scaler': None, 'classifier': Forest},
+    'kkkk': {'sample': 750000, 'scaler': None, 'classifier': Forest}
 }
+
+for k in params:
+    params[k]['ignore_features'] = None
 
 # use bayes for the zero-split
 params['ukuu']['classifier'] = Bayes
@@ -42,4 +42,4 @@ ensemble = ECEnsemble(train, test, params)
 print('transform for test')
 ensemble.transform()
 print('classify for test')
-ensemble.classify(dump_results=True, dump_name='test')
+ensemble.classify(dump_results=True, dump_name='quadtest-ensemble')
